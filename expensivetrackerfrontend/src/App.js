e13490e0876aa15bd1d4cf3141ec28858b2a7161
Murugan V <Murugan.V@mu-sigma.com>
2023-08-20 00:34:36 +0530

    updated app.js

--- expensivetrackerfrontend/src/App.js
+++ expensivetrackerfrontend/src/App.js
@@ -1,10 +1,33 @@
-import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+
+  const expenseData=[
+    {
+      title:"Car Insurance",
+      amount:200,
+      date: new Date(2022, 7, 22)
+    }, 
+     {
+      title:"Bike Insurance",
+      amount:200,
+      date: new Date(2022, 7, 22)
+    },
+    {
+      title:"Health Insurance",
+      amount:200,
+      date: new Date(2022, 7, 22)
+    },
+    {
+      title:"Gym Insurance",
+      amount:200,
+      date: new Date(2022, 7, 22)
+    },
+  ]
   return (
     <div className="App">
-      <div>Hello World</div>
-     
+    
+       <ExpenseItem />
     </div>
   );
 }
---
+++ expensivetrackerfrontend/src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import './styles/ExpenseItem.css'
+
+function ExpenseItem(){
+    return(
+        <div className='expense-item'>
+            <div className='expense-item__description'>July 22</div>
+            <div className='expense-item__description'>
+            <h2>Murugan</h2>
+            <div className='expense-item__price'>$2022</div>
+            </div>
+        </div>
+    
+    )
+}
+
+export default ExpenseItem;
