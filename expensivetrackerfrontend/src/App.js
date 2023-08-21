82ae66183be1a5054974886960ced899f5ccad71
Murugan V <Murugan.V@mu-sigma.com>
2023-08-22 02:10:20 +0530

    updated Expense form

--- expensivetrackerfrontend/src/App.js
+++ expensivetrackerfrontend/src/App.js
@@ -1,10 +1,12 @@
 import ExpenseData from './components/Expenses/ExpenseData';
+import { NewExpense } from './components/Expenses/NewExpense';
 
 function App() {
 
   
   return (
     <div className="App">
+      <NewExpense/>
       <ExpenseData/>
     </div>
   );
