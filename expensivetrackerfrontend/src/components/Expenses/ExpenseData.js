b78461ff755649545541d43bbc15a2aeffdd931a
Murugan V <Murugan.V@mu-sigma.com>
2023-10-09 01:10:34 +0530

    updated expense tracke

--- expensivetrackerfrontend/src/components/Expenses/ExpenseData.js
+++ expensivetrackerfrontend/src/components/Expenses/ExpenseData.js
@@ -55,10 +55,11 @@ function ExpenseData(){
             <ExpensesFilter selected={selectedFilteredYear} onFilteredYear={handleSelectedYear}/>
 
             <p>Data for years {filtersText} is Hidden</p>
-        <ExpenseItem title={expenseData[0].title} amount={expenseData[0].amount} date={expenseData[0].date} />
+        {/* <ExpenseItem title={expenseData[0].title} amount={expenseData[0].amount} date={expenseData[0].date} />
         <ExpenseItem  title={expenseData[1].title} amount={expenseData[1].amount} date={expenseData[1].date} />
         <ExpenseItem title={expenseData[2].title} amount={expenseData[2].amount} date={expenseData[2].date} />
-        <ExpenseItem title={expenseData[3].title} amount={expenseData[3].amount} date={expenseData[3].date}  />
+        <ExpenseItem title={expenseData[3].title} amount={expenseData[3].amount} date={expenseData[3].date}  /> */}
+        {expenseData.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>)}
         </div>
         </div>
 
